1ba6ea1637253a1a8d496487ece5acc8254e6b0e
Yousuf Maalik-Jaleel <ymaalikjaleel@mmw-ymaleekjale.cb.careerbuilder.com>
2018-02-22 21:09:55 +0000

    adding props as functions and binding

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ class App extends Component {
         ]
     }
 
-    switchNameHandler = () => {
+    switchNameHandler = (newName) => {
         this.setState({
             persons:  [
-                { name: 'Maximiian', age: 25},
+                { name: newName, age: 25},
                 { name: 'manu', age: 26},
                 { name: 'stefanuu', age: 29}
             ]
@@ -24,10 +24,15 @@ class App extends Component {
         return (
             <div className="App">
                 <h1>I am a react app</h1>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <button onClick={ () => this.switchNameHandler('nnnnnn') }>Switch Name</button>
 
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this, 'duper')}>
+                        my hobies: games
+                </Person>
                 <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
 
             </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <p>im {props.name} and i am {props.age} years old</p>
+            <p onClick={props.click}>im {props.name} and i am {props.age} years old</p>
             <p>{props.children}</p>
         </div>
     );
