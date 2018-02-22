063e38f28c0221f73b8b9bcd7c2a27c2148fcd34
Yousuf Maalik-Jaleel <ymaalikjaleel@mmw-ymaleekjale.cb.careerbuilder.com>
2018-02-22 14:02:27 +0000

    adding new states

--- src/App.js
+++ src/App.js
@@ -3,12 +3,33 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+    state = {
+        persons: [
+            { name: 'max', age: 25},
+            { name: 'manu', age: 26},
+            { name: 'stef', age: 21}
+        ]
+    }
+
+    switchNameHandler = () => {
+        this.setState({
+            persons:  [
+                { name: 'Maximiian', age: 25},
+                { name: 'manu', age: 26},
+                { name: 'stefanuu', age: 29}
+            ]
+        })
+    }
     render () {
         return (
             <div className="App">
                 <h1>I am a react app</h1>
-                <Person name="Yousuf" age="21" />
-                <Person name="Jon" age="26" >My Hobbies: Gaming</Person>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
+
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+
             </div>
         );
 
