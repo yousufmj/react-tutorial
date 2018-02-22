1ebfe33914fef255d2c6b70b69a829385e4a7e67
Yousuf Maalik-Jaleel <ymaalikjaleel@mmw-ymaleekjale.cb.careerbuilder.com>
2018-02-22 21:21:28 +0000

    changing state with input field

--- src/App.js
+++ src/App.js
@@ -20,17 +20,29 @@ class App extends Component {
             ]
         })
     }
+
+    nameChangedHandler = (event) => {
+        this.setState({
+            persons:  [
+                { name: 'Max', age: 25},
+                { name: event.target.value, age: 26},
+                { name: 'stefanuu', age: 29}
+            ]
+        })
+    }
     render () {
         return (
             <div className="App">
                 <h1>I am a react app</h1>
-                <button onClick={ () => this.switchNameHandler('nnnnnn') }>Switch Name</button>
+                <button onClick={ () => this.switchNameHandler('new name') }>Switch Name</button>
 
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
                 <Person
                     name={this.state.persons[1].name}
                     age={this.state.persons[1].age}
-                    click={this.switchNameHandler.bind(this, 'duper')}>
+                    click={this.switchNameHandler.bind(this, 'duper')}
+                    changed={this.nameChangedHandler}
+                >
                         my hobies: games
                 </Person>
                 <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,6 +5,7 @@ const person = (props) => {
         <div>
             <p onClick={props.click}>im {props.name} and i am {props.age} years old</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
     );
 };
